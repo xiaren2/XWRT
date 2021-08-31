47572d80ce374047c4f29c5fc10c1887e0bf1897
NueXini <34617094+NueXini@users.noreply.github.com>
2021-08-31 13:03:54 +0800

    fix

--- sh/shell-1.sh
+++ sh/shell-1.sh
@@ -2,9 +2,22 @@
 # 添加插件源
 sed -i '$a src-git NueXini_Packages https://github.com/NueXini/NueXini_Packages.git' feeds.conf.default
 
+# 设置路由器IP地址为192.168.3.1
+sed -i 's/192.168.1.*.1/192.168.3.1/g' package/base-files/files/bin/config_generate
+
+# lean默认设置
+if [ ! -f "package/lean/default-settings/files/zzz-default-settings" ]; then
+	sed -i 's#%D#&/NueXini#g' package/base-files/files/etc/openwrt_release
+else
+	sed -i "s#'OpenWrt#&/NueXini#g" package/lean/default-settings/files/zzz-default-settings
+fi
 
 # 删除lean libcryptopp
 if [ -d "package/lean/libcryptopp" ]; then
 	rm -rf package/lean/libcryptopp
 fi
 
+# Lienol
+if [ -f "package/default-settings/files/zzz-default-settings" ]; then
+	sed -i "s#'OpenWrt#&/NueXini#g" package/default-settings/files/zzz-default-settings
+fi
