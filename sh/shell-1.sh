7eef06b56549e7ea2ef20adcce90e759392d6d4c
NueXini <34617094+NueXini@users.noreply.github.com>
2021-08-31 15:50:45 +0800

    hi

--- sh/shell-1.sh
+++ sh/shell-1.sh
@@ -6,18 +6,19 @@ sed -i '$a src-git NueXini_Packages https://github.com/NueXini/NueXini_Packages.
 sed -i 's/192.168.1.*.1/192.168.3.1/g' package/base-files/files/bin/config_generate
 
 # lean默认设置
-if [ ! -f "package/lean/default-settings/files/zzz-default-settings" ]; then
-	sed -i 's#%D#&/NueXini#g' package/base-files/files/etc/openwrt_release
-else
+if [ -f "package/lean/default-settings/files/zzz-default-settings" ]; then
 	sed -i "s#'OpenWrt#&/NueXini#g" package/lean/default-settings/files/zzz-default-settings
+elif [ -f "package/lean/default-settings/files/zzz-default-settings" ]; then
+	sed -i "s#'OpenWrt#&/NueXini#g" package/default-settings/files/zzz-default-settings
+else
+	sed -i 's#%D#&/NueXini#g' package/base-files/files/etc/openwrt_release
+	sed -i 's#downloads.openwrt.org#mirrors.cloud.tencent.com/openwrt#g' /etc/opkg/distfeeds.conf
 fi
+###
 
 # 删除lean libcryptopp
 if [ -d "package/lean/libcryptopp" ]; then
 	rm -rf package/lean/libcryptopp
 fi
+###
 
-# Lienol
-if [ -f "package/default-settings/files/zzz-default-settings" ]; then
-	sed -i "s#'OpenWrt#&/NueXini#g" package/default-settings/files/zzz-default-settings
-fi
