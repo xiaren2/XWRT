0b6148969c76b3a7bde5f6c075a0dd42766cdfa5
NueXini <34617094+NueXini@users.noreply.github.com>
2021-08-31 13:01:19 +0800

    fix libcryptopp

--- sh/shell-1.sh
+++ sh/shell-1.sh
@@ -1,3 +1,10 @@
 
 # 添加插件源
 sed -i '$a src-git NueXini_Packages https://github.com/NueXini/NueXini_Packages.git' feeds.conf.default
+
+
+# 删除lean libcryptopp
+if [ -d "package/lean/libcryptopp" ]; then
+	rm -rf package/lean/libcryptopp
+fi
+
