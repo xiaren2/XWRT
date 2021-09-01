3a37f097722afbc943bd91cd3d030800d208b8c3
NueXini <34617094+NueXini@users.noreply.github.com>
2021-09-01 20:23:29 +0800

    install upx

--- sh/shell-2.sh
+++ sh/shell-2.sh
@@ -2,3 +2,6 @@
 # 设置luci-theme-argon为默认主题
 sed -i 's/luci-theme-bootstrap/luci-theme-argon/g' feeds/luci/collections/luci/Makefile
 
+# 安装UPX
+ln -s /usr/bin/upx ./staging_dir/host/bin/upx
+ln -s /usr/bin/upx-ucl ./staging_dir/host/bin/upx-ucl
