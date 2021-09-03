be9b247b9428119e816cb625a3104a10477ac977
NueXini <34617094+NueXini@users.noreply.github.com>
2021-09-03 10:41:10 +0800

    Update shell-2.sh

--- sh/shell-2.sh
+++ sh/shell-2.sh
@@ -7,5 +7,5 @@ ln -s /usr/bin/upx ./staging_dir/host/bin/upx
 ln -s /usr/bin/upx-ucl ./staging_dir/host/bin/upx-ucl
 
 # 
-rm -rf feeds/NueXini_Packages/luci-app-turboacc
-rm -rf feeds/NueXini_Packages/shortcut-fe
+#rm -rf feeds/NueXini_Packages/luci-app-turboacc
+#rm -rf feeds/NueXini_Packages/shortcut-fe
