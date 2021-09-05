01f3c7de90b81cd5593636d03c7f7a120b2a6e4e
NueXini <34617094+NueXini@users.noreply.github.com>
2021-09-05 23:27:45 +0800

    Update shell-2.sh

--- sh/shell-2.sh
+++ sh/shell-2.sh
@@ -1,7 +1,7 @@
 
 # 设置luci-theme-argon为默认主题
 #sed -i 's/luci-theme-bootstrap/luci-theme-argon/g' feeds/luci/collections/luci/Makefile
-sed -i 's/+luci-theme-bootstrap//g' feeds/luci/collections/luci/Makefile
+sed -i 's/+luci-theme-bootstrap //g' feeds/luci/collections/luci/Makefile
 
 # 安装UPX
 ln -s /usr/bin/upx ./staging_dir/host/bin/upx
