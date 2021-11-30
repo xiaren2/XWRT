a940c167e76b9097151196bb375721ac572625ba
NueXini <34617094+NueXini@users.noreply.github.com>
2021-11-30 22:24:54 +0800

    language_fix

--- sh/shell-2.sh
+++ sh/shell-2.sh
@@ -11,12 +11,13 @@ ln -s /usr/bin/upx-ucl ./staging_dir/host/bin/upx-ucl
 
 # x-wrt
 if [ -d "feeds/x" ]; then
-	sed -i "s#luci.main.lang='auto'#luci.main.lang='zh_cn'#g" feeds/x/base-config-setting/files/uci.defaults
+	sed -i "s#luci.main.lang='auto'#luci.main.lang='zh_Hans'#g" feeds/x/base-config-setting/files/uci.defaults
 	sed -i "s#PasswordAuth='off'#PasswordAuth='on'#g" feeds/x/base-config-setting/files/uci.defaults
 	sed -i "s#RootPasswordAuth='off'#RootPasswordAuth='on'#g" feeds/x/base-config-setting/files/uci.defaults
 	sed -i 's/-hidden//g' feeds/luci/applications/luci-app-opkg/root/usr/share/luci/menu.d/luci-app-opkg.json
 	sed -i 's/-hidden//g' feeds/luci/modules/luci-mod-system/root/usr/share/luci/menu.d/luci-mod-system.json
 	sed -i 's/-hidden//g' feeds/luci/modules/luci-mod-status/root/usr/share/luci/menu.d/luci-mod-status.json
 	sed -i 's#downloads.openwrt.org#mirrors.cloud.tencent.com/openwrt#g' include/version.mk
+	
 fi
 
