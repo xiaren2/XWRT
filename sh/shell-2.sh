68b300fd3a5afed0a8a80380f119904925b12f84
NueXini <34617094+NueXini@users.noreply.github.com>
2021-12-01 22:22:15 +0800

    luci.main.lang='zh_Hans'

--- sh/shell-2.sh
+++ sh/shell-2.sh
@@ -11,7 +11,7 @@ ln -s /usr/bin/upx-ucl ./staging_dir/host/bin/upx-ucl
 
 # x-wrt
 if [ -d "feeds/x" ]; then
-	sed -i "s#luci.main.lang='auto'#luci.main.lang='zh_cn'#g" feeds/x/base-config-setting/files/uci.defaults
+	sed -i "s#luci.main.lang='auto'#luci.main.lang='zh_Hans'#g" feeds/x/base-config-setting/files/uci.defaults
 	sed -i "s#PasswordAuth='off'#PasswordAuth='on'#g" feeds/x/base-config-setting/files/uci.defaults
 	sed -i "s#RootPasswordAuth='off'#RootPasswordAuth='on'#g" feeds/x/base-config-setting/files/uci.defaults
 	sed -i 's/-hidden//g' feeds/luci/applications/luci-app-opkg/root/usr/share/luci/menu.d/luci-app-opkg.json
