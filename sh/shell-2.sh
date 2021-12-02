a4c6f9adcdcb0de0d00b8f71e894fd4c893314ae
NueXini <34617094+NueXini@users.noreply.github.com>
2021-12-02 18:37:42 +0800

    Update shell-2.sh

--- sh/shell-2.sh
+++ sh/shell-2.sh
@@ -19,7 +19,6 @@ if [ -d "feeds/x" ]; then
 	sed -i 's/-hidden//g' feeds/luci/modules/luci-mod-system/root/usr/share/luci/menu.d/luci-mod-system.json
 	sed -i 's/-hidden//g' feeds/luci/modules/luci-mod-status/root/usr/share/luci/menu.d/luci-mod-status.json
 	sed -i 's#downloads.openwrt.org#mirrors.cloud.tencent.com/openwrt#g' include/version.mk
-	cd feeds/NueXini_Packages
 	curl -s https://raw.githubusercontent.com/NueXini/BuildOpenWrt/master/sh/NueXini_Language_Fix.sh | /bin/bash
 fi
 
