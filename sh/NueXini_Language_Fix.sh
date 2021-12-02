1257fe8a93a78decca19bd922ec371293548ece7
NueXini <34617094+NueXini@users.noreply.github.com>
2021-12-02 19:15:23 +0800

    Language_Fix

--- sh/NueXini_Language_Fix.sh
+++ sh/NueXini_Language_Fix.sh
@@ -1,1 +1,12 @@
+#!/bin/bash
+# Create By NueXini
 
+cd feeds/NueXini_Packages
+PATH_1="$({ find -type d|grep "zh-cn"; } 2>"/dev/null")"
+PATH_2="$({ dirname $PATH_1; } 2>"/dev/null")"
+
+
+for a in ${PATH_1}
+do
+    ln -s ${PATH_1} ${PATH_1}/zh_Hans
+done
