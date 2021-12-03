6cde134238dcf7ea734e0bf44dac34c7e8f68077
NueXini <34617094+NueXini@users.noreply.github.com>
2021-12-03 17:38:25 +0800

    Update uci.sh

--- sh/uci.sh
+++ sh/uci.sh
@@ -3,4 +3,5 @@
 
 FILE=$(cat $GITHUB_WORKSPACE/sh/uci.defaults)
 
-sed -e -i '$i \\$FILE' feeds/x/base-config-setting/files/uci.defaults
+sed -i '/exit 0/d' feeds/x/base-config-setting/files/uci.defaults
+echo "\$FILE" >> feeds/x/base-config-setting/files/uci.defaults
