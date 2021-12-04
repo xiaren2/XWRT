7e346c77df1961434642d03ffb8cf1c2cdc7a08c
NueXini <34617094+NueXini@users.noreply.github.com>
2021-12-04 17:24:13 +0800

    for x

--- sh/xxx.sh
+++ sh/xxx.sh
@@ -1,6 +1,11 @@
 #!/bin/bash
 # Create By NueXini
 
+# 修改luci默认启动页为 概览 admin/status/overview
+#sed -i 's,admin/initsetup,d' feeds/luci/modules/luci-base/luasrc/dispatcher.lua
+sed -i 's,/etc/config/wizard,,g' feeds/luci/modules/luci-base/luasrc/dispatcher.lua
+sed -i 's,admin/initsetup,admin/status/overview,g' feeds/luci/modules/luci-base/luasrc/dispatcher.lua
+
 # 显示系统进程 软件包 启动项
 sed -i 's/-hidden//g' feeds/luci/applications/luci-app-opkg/root/usr/share/luci/menu.d/luci-app-opkg.json
 sed -i 's/-hidden//g' feeds/luci/modules/luci-mod-system/root/usr/share/luci/menu.d/luci-mod-system.json
@@ -13,6 +18,7 @@ sed -i "s#luci.main.lang='auto'#luci.main.lang='zh_cn'#g" feeds/x/base-config-se
 sed -i "s#PasswordAuth='off'#PasswordAuth='on'#g" feeds/x/base-config-setting/files/uci.defaults
 sed -i "s#RootPasswordAuth='off'#RootPasswordAuth='on'#g" feeds/x/base-config-setting/files/uci.defaults
 
+###
 # Other  需写入uci.defaults的命令
 sed -i '/exit 0/d' feeds/x/base-config-setting/files/uci.defaults
 
