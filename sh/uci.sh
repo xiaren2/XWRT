2679a05eaa1f7066b355a9a82805986d00ece5d5
NueXini <34617094+NueXini@users.noreply.github.com>
2021-12-04 17:11:57 +0800

    Update uci.sh

--- sh/uci.sh
+++ sh/uci.sh
@@ -1,10 +1,15 @@
 #!/bin/bash
 # Create By NueXini
 
-#FILE=$(cat $GITHUB_WORKSPACE/sh/uci.defaults)
+# 设置系统语言为简体中文
+sed -i "s#luci.main.lang='auto'#luci.main.lang='zh_cn'#g" feeds/x/base-config-setting/files/uci.defaults
 
+#默认开启SSH
+sed -i "s#PasswordAuth='off'#PasswordAuth='on'#g" feeds/x/base-config-setting/files/uci.defaults
+sed -i "s#RootPasswordAuth='off'#RootPasswordAuth='on'#g" feeds/x/base-config-setting/files/uci.defaults
+
+# Other
 sed -i '/exit 0/d' feeds/x/base-config-setting/files/uci.defaults
-
 
 cat $GITHUB_WORKSPACE/sh/uci.defaults | while read line
 do
