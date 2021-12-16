fca09d5b885ef57a951fc1f5619a2f8ecb9ad169
NueXini <34617094+NueXini@users.noreply.github.com>
2021-12-16 23:05:42 +0800

    init

--- sh/Hi_NueXini.sh
+++ sh/Hi_NueXini.sh
@@ -1,23 +1,14 @@
 #!/bin/bash
 
-# 设置luci-theme-argon为默认主题
 #sed -i 's/luci-theme-bootstrap/luci-theme-argon/g' feeds/luci/collections/luci/Makefile
 sed -i 's/+luci-theme-bootstrap //g' feeds/luci/collections/luci/Makefile
 sed -i 's/192.168.1.*.1/192.168.3.1/g' feeds/luci/modules/luci-mod-system/htdocs/luci-static/resources/view/system/flash.js
 
-# 安装UPX
 ln -s /usr/bin/upx ./staging_dir/host/bin/upx
 ln -s /usr/bin/upx-ucl ./staging_dir/host/bin/upx-ucl
 
-# x-wrt
 if [ -d "feeds/x" ]; then
-	#curl -s https://raw.githubusercontent.com/NueXini/BuildOpenWrt/master/sh/uci.sh | /bin/bash
-	$GITHUB_WORKSPACE/sh/xxx.sh
-
-	cp -f $GITHUB_WORKSPACE/sh/banner package/base-files/files/etc/banner
-
-	cd feeds/NueXini_Packages
-	#curl -s https://raw.githubusercontent.com/NueXini/BuildOpenWrt/master/sh/language_fix.sh | /bin/bash
-	$GITHUB_WORKSPACE/sh/language_fix.sh
+	$GITHUB_WORKSPACE/sh/x.sh
+	
 fi
 
