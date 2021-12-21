8111c90be3ea7a5f7cde2016286851064b9728a4
NueXini <34617094+NueXini@users.noreply.github.com>
2021-12-22 07:38:27 +0800

    init

--- sh/Hi_NueXini.sh
+++ sh/Hi_NueXini.sh
@@ -6,7 +6,7 @@ sed -i 's/+luci-theme-bootstrap //g' feeds/luci/collections/luci/Makefile
 sed -i 's/192.168.1.*.1/192.168.3.1/g' feeds/luci/modules/luci-mod-system/htdocs/luci-static/resources/view/system/flash.js
 
 sed -i '/luciversion/d' feeds/luci/modules/luci-base/src/mkversion.sh
-sed -i '/luciname/aluciversion = "/ Compile on $(TZ=UTC+8 date "+%Y-%m-%d") by NueXini"' feeds/luci/modules/luci-base/src/mkversion.sh
+sed -i '/luciname/aluciversion = "compile on $(TZ=UTC+10 date "+%Y-%m-%d") by NueXini"' feeds/luci/modules/luci-base/src/mkversion.sh
 
 
 ln -s /usr/bin/upx ./staging_dir/host/bin/upx
