9ac14f2ae7c56d085bbb4467fcb10220ca24b8f2
NueXini <34617094+NueXini@users.noreply.github.com>
2022-01-03 21:28:09 +0800

    add path

--- sh/Hi_NueXini.sh
+++ sh/Hi_NueXini.sh
@@ -5,6 +5,7 @@ sed -i 's/+luci-theme-bootstrap //g' feeds/luci/collections/luci/Makefile
 
 sed -i 's/192.168.1.*.1/192.168.3.1/g' feeds/luci/modules/luci-mod-system/htdocs/luci-static/resources/view/system/flash.js
 
+# /usr/lib/lua/luci/version.lua
 sed -i 's/${3:-LuCI}/NueXini/g' feeds/luci/modules/luci-base/src/mkversion.sh
 sed -i 's/${2:-Git}/$(TZ=UTC-16 date "+%Y-%m-%d")/g' feeds/luci/modules/luci-base/src/mkversion.sh
 
