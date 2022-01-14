b1604226e3dd20fc18a7abbafe9c1aa1a1501502
NueXini <34617094+NueXini@users.noreply.github.com>
2022-01-14 13:11:01 +0800

    Update Hello_NueXini.sh

--- sh/Hello_NueXini.sh
+++ sh/Hello_NueXini.sh
@@ -7,6 +7,8 @@ echo 'src-git NueXini_Packages https://github.com/NueXini/NueXini_Packages.git' 
 
 sed -i 's/192.168.1.*.1/192.168.3.1/g' package/base-files/files/bin/config_generate
 
+sed -i 's/%R/NueXini/g' package/base-files/files/etc/openwrt_release
+
 sed -i 's,0.openwrt.pool.ntp.org,time1.cloud.tencent.com,g' package/base-files/files/bin/config_generate
 sed -i 's,1.openwrt.pool.ntp.org,ntp1.aliyun.com,g' package/base-files/files/bin/config_generate
 sed -i 's,2.openwrt.pool.ntp.org,ntp.ntsc.ac.cn,g' package/base-files/files/bin/config_generate
