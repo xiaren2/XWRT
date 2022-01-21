214494ae96064d16f8efb482f5b0a8b130d79ba3
NueXini <34617094+NueXini@users.noreply.github.com>
2022-01-21 19:11:43 +0800

    update NueXini

--- sh/z.make.sh
+++ sh/z.make.sh
@@ -3,19 +3,23 @@
 cd nuexini
 ../sh/Hello_NueXini.sh
 ./scripts/feeds update -a && ./scripts/feeds install -a
-[ -e ../config/${{ matrix.config }} ] && cp -rf ../config/${{ matrix.config }} ./.config
-../sh/Hi_NueXini.sh
-make defconfig
-make download -j8
-rm -rf $(find ./dl/ -size -1024c)
-make -j8 || make -j1 V=sc >> ./nuexini/make.log 2>&1
-time=$(TZ=UTC-8 date "+%H%M")
-zip -q -r Bin-NueXini-$time.zip ./bin/
-./sh/z.cowtransfer.sh
-./cowtransfer-uploader -s -p 64 Bin-NueXini-$time.zip 2>&1 | tee cowtransfer.log
-cp -rf Bin-NueXini-$time.zip ./nuexini/
-echo "::warning file=cowtransfer.com::$(cat cowtransfer.log | grep https)"
 
+ls ../config | grep .config | while read conf
+do
+    cp -rf ../config/$conf ./.config
+	../sh/Hi_NueXini.sh
+	make defconfig
+	make download -j8
+	rm -rf $(find ./dl/ -size -1024c)
+	make -j8 || make -j1 V=sc >> ./make.log 2>&1
+done
+
+time=$(TZ=UTC-8 date "+%H%M")
+zip -q -r NueXini-$time.zip ./bin/
+../sh/z.cowtransfer.sh
+./cowtransfer-uploader -s -p 64 NueXini-$time.zip 2>&1 | tee cowtransfer.log
+cp -rf ./bin/ ./nuexini/
+echo "::warning file=cowtransfer.com::$(cat cowtransfer.log | grep https)"
 
 # Creatr by NueXini
 
